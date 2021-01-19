816db1ef23ed4e69fc7731e3a8195fe1b49ca7b3
2013888096 <2013888096@qq.com>
2021-01-19 20:35:41 +0800

    '完成登入注册相关的界面'

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,15 @@
+import {defineComponent} from 'vue'
+// defineComponent 用于代码提示
+import {MailOutlined,UserOutlined,LockOutlined} from '@ant-design/icons-vue'
+
+export default defineComponent({
+  // 注册组件
+  components:{
+    UserOutlined,
+    LockOutlined,
+    MailOutlined
+  },
+  setup(){
+
+  }
+});
